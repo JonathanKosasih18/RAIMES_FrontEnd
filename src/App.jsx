2939b702e1abd2226138652e6451fa25c3ae7bde
Jonathan Kosasih <152432942+JonathanKosasih18@users.noreply.github.com>
2025-10-26 13:50:38 +0700

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import LoginPage from './pages/LoginPage';
 import QuestionnairePage from './pages/QuestionnairePage';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/questionnaire" replace />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/questionnaire" element={<QuestionnairePage />} />
         </Routes>
       </BrowserRouter>
